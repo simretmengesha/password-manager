986fa3a7ae3de141ef2d1b79c9fbdd6450e40688
simretmengesha <amen101mg@gmail.com>
2022-09-04 21:25:00 +0300

    password generator section

---
+++ main.py
@@ -0,0 +1,36 @@
+import json
+from random import choice, shuffle, randint
+from tkinter import *
+from tkinter import messagebox
+import pyperclip
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+
+
+def generate_password():
+    """
+    This function generates random password  that contains letters, numbers and special characters.
+    """
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = randint(8, 10)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
+
+    password_list = [choice(letters) for _ in range(nr_letters)]
+    password_list += [choice(symbols) for _ in range(nr_symbols)]
+    password_list += [choice(numbers) for _ in range(nr_numbers)]
+
+    shuffle(password_list)
+
+    new_password = "".join(password_list)
+    password_input.insert(0, new_password)
+    pyperclip.copy(new_password)
